d6c5125ed61243782f4162b539f2225c7099b55e
Alex Ardelean <alex@cloudfire.co>
2022-07-03 19:27:25 +0300

    Solve day 21 Generics

---
+++ HackerRank_30DaysOfCode/Day 21/Printer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.HackerRank_30DaysOfCode.Day_21
+{
+    class Printer
+    {
+        public static void PrintArray<T>(T[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+                Console.WriteLine(array[i]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ using DataStructuresAndAlgorithms.HackerRank_30DaysOfCode.Day_16;
 using DataStructuresAndAlgorithms.HackerRank_30DaysOfCode.Day_17;
 using DataStructuresAndAlgorithms.HackerRank_30DaysOfCode.Day_18;
 using DataStructuresAndAlgorithms.HackerRank_30DaysOfCode.Day_19;
+using DataStructuresAndAlgorithms.HackerRank_30DaysOfCode.Day_21;
 using DataStructuresAndAlgorithms.SortingAlgorithms;
 using System.Linq;
 
@@ -20,15 +21,32 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] { 23, 58, 100, 4, -4, 2, 1000, 543, 0, 20, 20 };
-            //int[] array = new int[] { 8, 2, 4, 1, 3 };
-            //BubbleSort.Sort(array);
-            //BubbleSort.SortDesc(array);
-            //SelectionSort.Sort(array);
-            InsertionSort.Sort(array);
+            //int[] array = new int[] { 23, 58, 100, 4, -4, 2, 1000, 543, 0, 20, 20 };
+            ////int[] array = new int[] { 8, 2, 4, 1, 3 };
+            ////BubbleSort.Sort(array);
+            ////BubbleSort.SortDesc(array);
+            ////SelectionSort.Sort(array);
+            ////InsertionSort.Sort(array);
 
-            foreach (var element in array)
-                Console.WriteLine(element);
+            //foreach (var element in array)
+            //    Console.WriteLine(element);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] stringArray = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                stringArray[i] = Console.ReadLine();
+            }
+
+            Printer.PrintArray<Int32>(intArray);
+            Printer.PrintArray<String>(stringArray);
         }
     }
 }
